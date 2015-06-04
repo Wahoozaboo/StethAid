66d88324526597085b851dc0a8cfe26ae69eb1d2
Administrator <admin@Jamess-MacBook-Air.local>
2015-06-04 11:18:06 -0400

    Initial Commit

---
+++ Stethaid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stethaid
+//
+//  Created by Administrator on 6/4/15.
+//  Copyright (c) 2015 Sheikh Zayed Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stethaid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stethaid
+//
+//  Created by Administrator on 6/4/15.
+//  Copyright (c) 2015 Sheikh Zayed Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
